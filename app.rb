526986d162f0a7bc934a2d56d86cfa43e995edfd
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-19 07:47:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,22 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		"<h1>Hello World</h1>"
 	end
+	
+	 #get '/' do
+  #     erb :index
+  #   end
+    
+  #   get "/info" do
+  #     "Testing the info page"
+  #   end
+    
+  get '/' do
+      erb :index
+    end
+ 
+    get "/info" do
+      erb :info
+    end
 end
