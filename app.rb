e987f25bef459c402b4e5e6b3bc1c93d4e773472
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-19 07:49:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,23 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		"<h1>Hello World</h1>"
-	end
-	
-	 #get '/' do
-  #     erb :index
-  #   end
-    
-  #   get "/info" do
-  #     "Testing the info page"
-  #   end
-    
-  get '/' do
+	 get '/' do
       erb :index
     end
  
     get "/info" do
       erb :info
     end
+    
 end
